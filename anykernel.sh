bcbeddbd320788d3200e6a63b2faebc48233de25
Revanth Strakz <dr.revanthstrakz@gmail.com>
2018-04-05 17:53:47 +0530

    Stormguard

--- anykernel.sh
+++ anykernel.sh
@@ -4,32 +4,38 @@
 ## AnyKernel setup
 # begin properties
 properties() {
-kernel.string=NATO66613 @ xda-developers
+kernel.string=Stormguard Kernel for AOSP/LOS Custom ROMs
 do.devicecheck=1
-do.modules=1
+do.modules=0
 do.cleanup=1
-do.cleanuponabort=0
+do.cleanuponabort=1
 device.name1=mido
-device.name2=redmi note 4
-device.name3=Rendi Nude 4
-device.name4=Redmi Note 4
-device.name5=Redmi Note 4x
 } # end properties
 
 # shell variables
-block=/dev/block/bootdevice/by-name/boot;
+block=/dev/block/mmcblk0p21;
+ramdisk_compression=auto;
+is_slot_device=0;
+
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . /tmp/anykernel/tools/ak2-core.sh;
 
 
-## AnyKernel permissions
-# set permissions for included ramdisk files
-chmod -R 755 $ramdisk
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+chmod -R 750 $ramdisk/*;
+chown -R root:root $ramdisk/*;
+
 
 ## AnyKernel install
 dump_boot;
+
+# begin ramdisk changes
+
+# end ramdisk changes
+
 write_boot;
 
 ## end install
